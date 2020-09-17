4d4b2be87d2d65574d668ab72d33d5bd0acbf16b
Alluisio Silva <alluisio.silva@ipdec.org>
2020-09-17 13:41:38 -0400

    Finalização do curso de React - hooks e contexto

--- src/contexts/ValidacoesCadastro.js
+++ src/contexts/ValidacoesCadastro.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
 
-const ValidacoesCadastro = React.createContext();
+const ValidacoesCadastro = React.createContext({
+  cpf: semValidacao,
+  senha: semValidacao,
+  nome: semValidacao,
+});
+
+function semValidacao(dados) {
+  console.log(dados);
+  return { valido: true, texo: "" };
+}
 
 export default ValidacoesCadastro;
